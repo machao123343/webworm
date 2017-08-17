639206d597b619bf70a09a06c1c468e56057556a
machao123343 <79047302@qq.com>
2017-08-17 21:39:34 +0800

    修改base

--- base/data.go
+++ base/data.go
@@ -70,24 +70,7 @@ func (item Item) Valid() bool {
 
 //func Error() string
 
-//爬虫错误的接口
-type CrawlerError interface {
-	Type() ErrorType //获得错误类型
-	Error() string //获得错误提示信息
-}
 
-type myCrawlerError struct {
-	errType  ErrorType //错误类型
-	errMsg   string    //错误提示信息
-	fullErrMsg string  //完整的错误提示信息
-}
-
-//错误提示常量
-const (
-	DOWNLOADER_ERROR   ErrorType = "Downloader Error"
-	ANALYZER_ERROR     ErrorType = "Analyzer Error"
-	ITEM_PROCESSOR_ERROR ErrorType = "Item Processor Error"
-)
 
 
 
